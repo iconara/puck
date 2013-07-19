4b83e2f7968aa10db5bbf70476d2979542bd2004
Theo <theo@iconara.net>
2013-07-19 16:20:59 +0200

    Added simplecov, yardoc, travis-lint and coveralls

--- Gemfile
+++ Gemfile
@@ -2,7 +2,16 @@ source 'https://rubygems.org/'
 
 gemspec
 
+group :development do
+  gem 'pry'
+  gem 'travis-lint'
+  gem 'yard'
+  gem 'kramdown'
+end
+
 group :test do
   gem 'jruby-jars', '= 1.7.3'
   gem 'rspec'
+  gem 'simplecov'
+  gem 'coveralls'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,21 @@
 
 require 'bundler/setup'
 require 'tmpdir'
+
+unless ENV['COVERAGE'] == 'no'
+  require 'coveralls'
+  require 'simplecov'
+
+  if ENV.include?('TRAVIS')
+    Coveralls.wear!
+    SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+  end
+
+  SimpleCov.start do
+    add_group 'Source', 'lib'
+    add_group 'Unit tests', 'spec/puck'
+    add_group 'Integration tests', 'spec/integration'
+  end
+end
+
 require 'puck'
