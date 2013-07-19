e537f0f9f8149a1b308936ed41c4dc3a4a0c395e
Theo <theo@iconara.net>
2013-07-19 18:39:41 +0200

    Cleaner bootstrap

--- lib/puck/bootstrap.rb
+++ lib/puck/bootstrap.rb
@@ -1,15 +1,12 @@
 if ARGV.any?
   file_name = ARGV.shift
-  found = false
   PUCK_BIN_PATH.each do |dir|
     path = __FILE__.sub('jar-bootstrap.rb', File.join(dir, file_name))
     if File.exists?(path)
-      found = true
       load(path)
+      return
     end
   end
-  unless found
-    abort(%(No "#{file_name}" in #{PUCK_BIN_PATH.join(File::PATH_SEPARATOR)}))
-  end
+  abort(%(No "#{file_name}" in #{PUCK_BIN_PATH.join(File::PATH_SEPARATOR)}))
 end
  
