c085960354c58253881287bc22f8d80efcf5c3ad
Theo <theo@iconara.net>
2013-07-19 17:14:12 +0200

    Changed the error message when the first argument is not a bin file

--- lib/puck/bootstrap.rb
+++ lib/puck/bootstrap.rb
@@ -1,10 +1,10 @@
 if ARGV.any?
   file_name = ARGV.shift
-  bootstrap_path = __FILE__.sub('jar-bootstrap.rb', "META-INF/app.home/bin/#{file_name}")
-  begin
-    load(bootstrap_path)
-  rescue LoadError => e
-    abort(e.message)
+  file_path = __FILE__.sub('jar-bootstrap.rb', "META-INF/app.home/bin/#{file_name}")
+  if File.exists?(file_path)
+    load(file_path)
+  else
+    abort("No #{file_name} in classpath:META-INF/app.home/bin")
   end
 end
  
