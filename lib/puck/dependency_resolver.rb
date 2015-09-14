9279297ff79530dd203c0c8c5bab949bd68ab621
Mathias Söderberg <mathias.soederberg@gmail.com>
2015-09-14 13:29:01 +0200

    Set BUNDLE_GEMFILE for scripting container
    Rather than setting the current directory to `app_dir` we'll set
    `BUNDLE_GEMFILE` to the correct Gemfile. I don't understand Bundler
    at all, and I'm not sure why this is better than setting the current
    directory, but this solves the issue with fewer changes (and probably
    less side effects).

--- lib/puck/dependency_resolver.rb
+++ lib/puck/dependency_resolver.rb
@@ -5,13 +5,12 @@ module Puck
   # @private
   class DependencyResolver
     def resolve_gem_dependencies(options = {})
-      app_dir = options[:app_dir] || Dir.pwd
       gem_home = options[:gem_home] || ENV['GEM_HOME']
-      gemfile = options[:gemfile] || File.expand_path('Gemfile', app_dir)
+      gemfile = options[:gemfile] || File.expand_path('Gemfile', options[:app_dir] || Dir.pwd)
       lockfile = options[:lockfile] || "#{gemfile}.lock"
       groups = options[:gem_groups] || [:default]
 
-      bundler_specs = contained_bundler(gem_home, gemfile, lockfile, groups, app_dir)
+      bundler_specs = contained_bundler(gem_home, gemfile, lockfile, groups)
       bundler_specs.delete_if { |spec| spec[:name] == 'bundler' }
       bundler_specs.map do |spec|
         base_path = spec[:full_gem_path].chomp('/')
@@ -33,12 +32,12 @@ module Puck
 
     private
 
-    def contained_bundler(gem_home, gemfile, lockfile, groups, app_dir)
+    def contained_bundler(gem_home, gemfile, lockfile, groups)
       Bundler.with_clean_env do
         scripting_container = Java::OrgJrubyEmbed::ScriptingContainer.new(Java::OrgJrubyEmbed::LocalContextScope::SINGLETHREAD)
         scripting_container.compat_version = Java::OrgJruby::CompatVersion::RUBY1_9
-        scripting_container.current_directory = app_dir
-        scripting_container.environment = Hash[ENV.merge('GEM_HOME' => gem_home).map { |k,v| [k.to_java, v.to_java] }]
+        scripting_container.current_directory = Dir.pwd
+        scripting_container.environment = Hash[ENV.merge('GEM_HOME' => gem_home, 'BUNDLE_GEMFILE' => gemfile).map { |k,v| [k.to_java, v.to_java] }]
         scripting_container.put('arguments', Marshal.dump([gemfile, lockfile, groups]).to_java_bytes)
         begin
           line = __LINE__ + 1 # as __LINE__ represents next statement line i JRuby, and that becomes difficult to offset
