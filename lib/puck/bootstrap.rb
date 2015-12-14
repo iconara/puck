4d4418c7501077734399d83904a5c1d00a3dba6a
Joel Segerlind <joel.segerlind@gmail.com>
2015-12-14 13:27:31 +0100

    Don't do Kernel.exit from bootstrap.rb
    It's not obvious that we want to exit just because the thread that ran jar-bootstrap.rb has finished.

--- lib/puck/bootstrap.rb
+++ lib/puck/bootstrap.rb
@@ -1,12 +1,16 @@
 if ARGV.any?
   file_name = ARGV.shift
+  bin_file_found = false
   PUCK_BIN_PATH.each do |dir|
     relative_path = File.join(dir, file_name)
     if File.exists?("classpath:/#{relative_path}")
+      bin_file_found = true
       $0 = relative_path
       load(relative_path)
-      exit
+      break
     end
   end
-  abort(%(No "#{file_name}" in #{PUCK_BIN_PATH.join(File::PATH_SEPARATOR)}))
+  unless bin_file_found
+    abort(%(No "#{file_name}" in #{PUCK_BIN_PATH.join(File::PATH_SEPARATOR)}))
+  end
 end
